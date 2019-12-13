aea3aa0dddf932944aa2cb31225d16edd3a4fa6c
Qi Cheng <41558367+Cheng-qi@users.noreply.github.com>
2019-12-14 01:12:34 +0800

    SSoNN
    SLFN_SSONN算法实现

---
+++ model1212.py
@@ -0,0 +1,380 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: 3.7.2
+@author: Qi Cheng
+@license: Apache Licence 
+@contact: chengqi96@126.com
+@site: https://github.com/Cheng-qi
+@software: PyCharm
+@file: model1123a.py
+@time: 2019/11/21 17:23
+"""
+import time
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+from pso20191127 import pso
+import pandas as pd
+import copy
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score, accuracy_score, matthews_corrcoef
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from functools import partial
+from tensorflow.examples.tutorials.mnist import input_data
+
+class SelfOrganizeNN(torch.nn.Module):
+
+    ## 初始化结构参数
+    def __init__(self,n_feature,n_out,begin_n_hidden=1, h_activaty_func = torch.relu, lossfunc=torch.nn.CrossEntropyLoss(),task = "classification"):
+        super(SelfOrganizeNN,self).__init__()
+        # self.inputs = torch.tensor(trains)
+
+        # self.outs = torch.tensor(exp_outs)
+        self.record_hidden = []
+        self.task = task
+        self.n_feature = n_feature
+        self.n_hidden = begin_n_hidden
+        self.n_output = n_out
+        self.h_actfunc = h_activaty_func
+        w_1 = torch.rand(self.n_feature, self.n_hidden)*2-1
+        b_1 = torch.rand(1,self.n_hidden)*2-1
+        # w_1 = torch.ones(self.n_feature, self.n_hidden)
+        # b_1 = torch.ones(1,self.n_hidden)
+        self.w_1 = Variable(w_1, requires_grad=True)
+        self.b_1 = Variable(b_1, requires_grad=True)
+        w_2 = torch.rand(self.n_hidden,self.n_output)*2-1
+        b_2 = torch.rand(1,self.n_output)*2-1
+        # w_2 = torch.ones(self.n_hidden,self.n_output)
+        # b_2 = torch.ones(1,self.n_output)
+        self.w_2 = Variable(w_2, requires_grad=True)
+        self.b_2 = Variable(b_2, requires_grad=True)
+        self.trainable_paras = [self.w_1, self.w_2, self.b_1, self.b_2]
+        self.lossfunc = lossfunc
+        self.loss_record =[]
+        self.growth_num = 0
+
+    ##喂入数据
+    def placeholder(self,x, y):
+        self.inputs = torch.tensor(x,dtype=torch.float32).reshape(-1,self.n_feature)
+        if type(y)==np.ndarray:
+            if self.task == "classification":
+                if y.ndim>1:
+                    _, y = np.where(y == 1)
+                self.exp_outs =torch.tensor(y,dtype=torch.long)
+            elif self.task == "Regression":
+                self.exp_outs = torch.tensor(y, dtype=torch.float32).reshape(-1, self.n_output)
+        else:
+            self.exp_outs = 0
+        return self.inputs,self.exp_outs
+
+    ###前向传播，定义损失函数
+    def forward(self, weight_decay=0.0001, train = True):
+        self.h_1 = self.h_actfunc(torch.matmul(self.inputs,self.w_1)+self.b_1)
+        self.h_2 = torch.matmul(self.h_1, self.w_2)+self.b_2
+        self.outs = F.softmax(self.h_2, dim=1)
+        if train:
+            self.loss = self.lossfunc(self.outs, self.exp_outs)  #无正则项
+            for para_i in self.trainable_paras:
+                self.loss += weight_decay*torch.norm(para_i)**2   #添加L2正则项
+        return self.loss
+
+
+    ##结构更新方法
+    def _update_structure(self, pso_fun=pso,structuring_form = "growing",):
+        def object(x):
+            w_1 = self.w_1.data.numpy()
+            w_2 = self.w_2.data.numpy()
+            b_1 = self.b_1.data.numpy()
+
+            dw_1 = x[:,:self.n_feature]
+            dw_2 = x[:,self.n_feature:(self.n_feature+self.n_output)]
+            db_1 = x[:,-1]
+
+            objects = np.zeros(shape=(x.shape[0]))
+            for pos_i in range(x.shape[0]):
+                w_1_tem = np.concatenate((w_1, dw_1[pos_i].T.reshape(-1,1)),axis=1)
+                w_2_tem = np.concatenate((w_2, dw_2[pos_i].T.reshape(1,-1)),axis=0)
+                b_1_tem = np.concatenate((b_1, db_1[pos_i].T.reshape(-1,1)),axis=1)
+
+                self.w_1.data = torch.tensor(w_1_tem, dtype=torch.float32)
+                self.w_2.data = torch.tensor(w_2_tem,dtype=torch.float32)
+                self.b_1.data = torch.tensor(b_1_tem,dtype=torch.float32)
+
+                self.forward()
+                objects[pos_i] = self.loss.data.numpy().reshape(1)
+                self.w_1.data = torch.tensor(w_1)
+                self.w_2.data = torch.tensor(w_2)
+                self.b_1.data = torch.tensor(b_1)
+            return objects
+
+        # 存储结构变化前的参数值
+        para_store = []
+        for para_i in self.trainable_paras:
+            para_store.append(para_i.data.numpy())
+
+        # 结构增长阶段
+        if structuring_form=="growing" :
+            lb = [-1]*(self.n_feature+self.n_output+1) ##粒子群搜索限定的范围
+            ub = [1]*(self.n_feature+self.n_output+1)
+            # g,fg = pso(object, lb, ub)
+            # g,fg = pso_fun(object, lb, ub, swarmsize = 50)
+            g,fg = pso_fun(object, lb, ub, swarmsize = 2*(self.n_feature+self.n_output+1))
+
+            # if fg < self.loss_record[-1]: #如果有效
+
+            if self.loss_record[-1]-fg>self.loss_record[-1]*0.001: #如果有效
+                self.growth_num = 0
+
+                # 待添加节点的参数
+                dw_1 = g[:self.n_feature]
+                dw_2 = g[self.n_feature:(self.n_feature + self.n_output)]
+                db_1 = g[-1]
+                # db_2 = g[-1]
+
+                # 更新节点参数
+                w_1_tem = np.concatenate((para_store[0], dw_1.T.reshape(-1,1)), axis=1)
+                w_2_tem = np.concatenate((para_store[1], dw_2.T.reshape(1,-1)), axis=0)
+                b_1_tem = np.concatenate((para_store[2], db_1.T.reshape(-1,1)), axis=1)
+
+                # 按照新节点重新构造网络
+                self.w_1 = Variable(torch.tensor(w_1_tem, dtype=torch.float32), requires_grad=True)
+                self.b_1 = Variable(torch.tensor(b_1_tem, dtype=torch.float32), requires_grad=True)
+                self.w_2 = Variable(torch.tensor(w_2_tem, dtype=torch.float32), requires_grad=True)
+                self.trainable_paras = [self.w_1, self.w_2, self.b_1, self.b_2]
+                self.opt = torch.optim.SGD(self.trainable_paras, lr=self.lr)  # 标准梯度优化器
+
+            else:
+                self.growth_num +=1
+
+        # 节点合并
+        elif structuring_form=="pruning":
+
+            # 取出参数值
+            w_1_tem = self.w_1.data.numpy()
+            w_2_tem = self.w_2.data.numpy()
+            b_1_tem = self.b_1.data.numpy()
+
+            ##合并w1和w_b,计算w_b最小距离的隐层节点，记录在min_index
+            wb_1_tem = np.concatenate((w_1_tem, b_1_tem), axis=0)
+            # w_1_dist0 = (w_1_tem**2).sum(axis=1)
+            wb_1_dist0 = np.linalg.norm(wb_1_tem, axis=0)**2
+            wb_1_dist1 = np.repeat(wb_1_dist0, wb_1_tem.shape[1]).reshape(wb_1_tem.shape[1],wb_1_tem.shape[1])
+            wb_1_dist2 = wb_1_dist1.T
+            wb_1_dist3 = wb_1_tem.T.dot(wb_1_tem)
+            wb_1_dist4 = wb_1_dist1+wb_1_dist2-2*wb_1_dist3
+            # wb_1_dist4 = np.clip(wb_1_dist4,1e-6,1e7)
+            one_trip = np.ones_like(wb_1_dist4)-np.tril(np.ones_like(wb_1_dist4))
+            wb_1_dist = (one_trip*wb_1_dist4)**0.5
+            wb_1_dist_mean = np.mean(wb_1_dist)*2*wb_1_dist.shape[0]/(wb_1_dist.shape[0]-1)
+            wb_1_dist_mean_tril = wb_1_dist+np.tril(np.ones_like(wb_1_dist)*wb_1_dist_mean)
+            wb_1_dist_std = wb_1_dist_mean_tril.std()*2*wb_1_dist.shape[0]/(wb_1_dist.shape[0]-1)
+            wb_1_dist[np.where(wb_1_dist==0)] = np.inf
+
+            if np.min(wb_1_dist) < wb_1_dist_mean - 1.5 * wb_1_dist_std:
+                min_index = np.unravel_index(np.argmin(wb_1_dist),wb_1_dist.shape)
+
+                # 计算更新后的wb_1
+                wb_1_dnew = np.mean(wb_1_tem[:,min_index],axis = 1)    #.reshape(w_1_tem.shape[0],-1)
+                wb_1_new = copy.deepcopy(wb_1_tem)
+                wb_1_new[:,min_index[0]] = wb_1_dnew
+                wb_1_new = np.delete(wb_1_new, min_index[1], axis=1)
+
+                # 更新后的参数
+                w_1_new = wb_1_new[:-1,:]
+                b_1_new = wb_1_new[-1,:].reshape(1,-1)
+                w_2_new = copy.deepcopy(w_2_tem)
+                w_2_new[min_index[0],:] = w_2_tem[min_index[0],:]+w_2_tem[min_index[1],:]
+                # w_2_new[min_index[1],:] = 0
+                w_2_new = np.delete(w_2_new, min_index[1], axis=0)
+
+                # 重新构造网络
+                self.w_1 = Variable(torch.tensor(w_1_new, dtype=torch.float32), requires_grad=True)
+                self.b_1 = Variable(torch.tensor(b_1_new, dtype=torch.float32), requires_grad=True)
+                self.w_2 = Variable(torch.tensor(w_2_new, dtype=torch.float32), requires_grad=True)
+                self.trainable_paras = [self.w_1, self.w_2, self.b_1, self.b_2]
+                self.opt = torch.optim.SGD(self.trainable_paras, lr=self.lr)  # 标准梯度优化器
+
+        # 更改隐含层节点数
+        self.n_hidden = self.w_1.data.shape[1]
+        # print(fg)
+        # print(self.forward())  # 前向传播
+        self.forward()  # 前向传播
+        return self.n_hidden
+
+
+    def trains(self,train_data, train_labels, learning_rate = 0.1, maxiter = 300, self_structure = True, pso_fun = pso,batch_size=3000):
+        # self.placeholder(train_data, train_labels)
+        self.lr = learning_rate
+        self.opt = torch.optim.Adam(self.trainable_paras, lr=self.lr)
+        # self.opt = torch.optim.Adam([self.w_1, self.b_1, self.w_2, self.b_2], lr=learning_rate)  #Adam优化器
+        # _ = self.forward()  # 前向传播
+        batchs_num = int(np.ceil(train_data.shape[0]/batch_size))
+        for it in range(maxiter):
+            for batch_i in range(batchs_num):
+                if batch_i != batchs_num-1:
+                    self.placeholder(train_data[batch_i:(batch_size+batch_i)], train_labels[batch_i:(batch_size+batch_i)])
+                else:
+                    self.placeholder(train_data[batch_i:], train_labels[batch_i:])
+            # start = time.time()
+            # self.forward()#前向传播
+                self.forward()  # 前向传播
+
+                self.loss_record.append(self.loss.data.numpy())
+                # print("变化前" , self.loss.data)  # 打印损失函数值
+
+                # 是否是自组织结构
+                # if self_structure and (it+1)%4==0:
+                if self_structure and it>5:
+
+                    #判断是否平滑 dx(k-2)-dx(k-1)<dx(k-1)-dx(k) 其中：dx(n) = x(n-1)-x(n)    即x(k-3)-3(x(k-2)-x(k-1))-x(k)<0
+                    if self.loss_record[-1-3]-3*(self.loss_record[-1-2]-self.loss_record[-1-1])-self.loss_record[-1]<0:
+
+                        # if it < maxiter/2 and self.growth_num<6: ##前半阶段，增长，如果超过5次增长无效，则停止增长
+                        if self.growth_num<3: ##前半阶段，增长，如果超过3次增长无效，则停止增长
+                            # print("增长")
+                            self._update_structure(pso_fun=pso_fun,structuring_form="growing")#是否是结构自组织神经网络
+                        else: #it> maxiter/2 : ##后半阶段， 合并
+                            # print("合并")
+                            self._update_structure(structuring_form="pruning")#是否是结构自组织神经网络
+                # print(time.time()-start)
+                self.loss.backward() #误差反向传播
+                self.opt.step()
+                self.opt.zero_grad()
+            # print("第%d次迭代"%it,self.loss.data) #打印损失函数值
+            self.record_hidden.append(self.n_hidden)
+        return self.loss.data
+
+    def predict(self,input,exp_out):
+        self.placeholder(input, exp_out)
+        pre_loss = self.forward(train=False)
+        self.pre_one_hot = np.argmax(self.outs.data.numpy(),1)
+        return self.pre_one_hot, pre_loss
+
+    def predict_prob(self,input,exp_out):
+        self.placeholder(input, exp_out)
+        self.forward(train=False)
+        return self.outs.data
+
+    def evaluate(self):
+        pre_one_hot = np.argmax(self.outs.data.numpy(),1)
+        exp_one_hot = self.exp_outs.numpy()
+        is_acc_sample = np.array(exp_one_hot==pre_one_hot,dtype=np.int)
+        self.accuracy = is_acc_sample.mean()
+        return self.accuracy
+##作图函数
+def plot(y,xlabel = "Iter",ylabel = "隐层节点个数", title="隐层节点个数迭代图"):
+    fig = plt.figure(figsize=(10, 6))
+    x = [i for i in range(len(y))]
+    plt.plot(x, y, 'r-', linewidth=1.5, markersize=5)
+    plt.title(title)
+    plt.xlabel(xlabel, fontsize=18)
+    plt.ylabel(ylabel, fontsize=18)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlim(0, )
+    plt.grid(True)
+    plt.show()
+
+
+if __name__=="__main__":
+    # np.random.seed(0)
+
+    # _, y = np.where(y==1)# y = (np.sum(x**2+np.exp(x),1)-np.mean(np.sum(x**2+np.exp(x),1)))/np.var(np.sum(x**2+np.exp(x),1))
+
+    # y_wine=np.array(pd.read_excel("../小论文程序与结果/数据/新酒分类/新酒分类.xls",sheet_name="训练数据",usecols=range(13,16),header = None))
+    # _, y_wine = np.where(y_wine==1)# y = (np.sum(x**2+np.exp(x),1)-np.mean(np.sum(x**2+np.exp(x),1)))/np.var(np.sum(x**2+np.exp(x),1))
+    #
+    print("************鸢尾花*************")
+
+    # 鸢尾花数据
+    x_flower = np.array(pd.read_excel("../小论文程序与结果/数据/新鸢尾属植物/归一化后数据.xls",sheet_name="训练数据各40组",usecols=[0,1,2,3],header = None))
+    y_flower = np.array(pd.read_excel("../小论文程序与结果/数据/新鸢尾属植物/归一化后数据.xls",sheet_name="训练数据各40组",usecols=[4,5,6],header = None))
+    x_flower_test = np.array(pd.read_excel("../小论文程序与结果/数据/新鸢尾属植物/归一化后数据.xls",sheet_name="测试数据各10组",usecols=[0,1,2,3],header = None))
+    y_flower_test = np.array(pd.read_excel("../小论文程序与结果/数据/新鸢尾属植物/归一化后数据.xls",sheet_name="测试数据各10组",usecols=[4,5,6],header = None))
+
+    model = SelfOrganizeNN(x_flower.shape[1],y_flower.shape[1],1,h_activaty_func=torch.relu)
+    # opt_SGD = torch.optim.SGD(model.parameters(), lr=0.01)
+    outs = model.trains(x_flower ,y_flower,maxiter=300,learning_rate = 0.1, self_structure = True)
+    ##训练集
+    model.predict(x_flower,y_flower)
+    print("训练集准确率",model.evaluate())
+    ##测试集
+    model.predict(x_flower_test,y_flower_test)
+    print("测试集准确率",model.evaluate())
+
+    print(outs)
+    print(model.n_hidden)
+    plot(np.array(model.record_hidden))
+
+
+
+    # print("************新酒*************")
+    # #新酒数据
+    # x_wine_pro = np.array(pd.read_excel("../小论文程序与结果/数据/新酒分类/新酒分类.xls",sheet_name="训练数据",usecols=range(0,13),header = None))
+    # y_wine = np.array(pd.read_excel("../小论文程序与结果/数据/新酒分类/新酒分类.xls", sheet_name="训练数据", usecols=range(13, 16), header=None))
+    # x_wine = (x_wine_pro- x_wine_pro.mean(axis=0)) / x_wine_pro.std(axis=0)  ##此处标准差分母为n维
+    #
+    # x_wine_pro_test = np.array(pd.read_excel("../小论文程序与结果/数据/新酒分类/新酒分类.xls",sheet_name="测试数据",usecols=range(0,13),header = None))
+    # y_wine_test = np.array(pd.read_excel("../小论文程序与结果/数据/新酒分类/新酒分类.xls", sheet_name="测试数据", usecols=range(13, 16), header=None))
+    # x_wine_test = (x_wine_pro_test-x_wine_pro.mean(axis=0))/x_wine_pro.std(axis=0) ##此处标准差分母为n维
+    #
+    #
+    # model_wine = SelfOrganizeNN(x_wine.shape[1], y_wine.shape[1],1,h_activaty_func=torch.relu)
+    # # opt_SGD = torch.optim.SGD(model.parameters(), lr=0.01)
+    # outs_wine = model_wine.trains(x_wine, y_wine, maxiter=200, learning_rate=0.1, self_structure = True)
+    #
+    #
+    # model_wine.predict(x_wine, y_wine)
+    # print("训练集准确率",model_wine.evaluate())
+    #
+    # model_wine.predict(x_wine_test, y_wine_test)
+    # print("测试集准确率",model_wine.evaluate())
+    # # print(outs)
+    # plot(np.array(model_wine.record_hidden))
+    # print(model_wine.n_hidden)
+    # print(x_wine.shape)
+    #
+    #
+     #新酒数据
+
+    # # 语音信号
+    # path = "../data/voice.data"
+    # voice_data = pd.read_table(path, header=None, sep=",")
+    # x_voice = voice_data[list(range(1,25))]
+    # y_voice = pd.Categorical(voice_data[0]).codes
+    # x_data = x_voice
+    # y_data = y_voice
+    # result_folder = "../results/results_voice"
+    #
+    # # 数据分割
+    # x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=0.8, stratify=y_data,
+    #                                                                         random_state=0)
+    # # 归一化数据
+    # stand = StandardScaler()
+    # stand.fit(x_train)
+    # x_train_std = stand.transform(x_train)
+    # x_test_std = stand.transform(x_test)
+    #
+    #
+    # model_voice = SelfOrganizeNN(x_train_std.shape[1], np.max(y_train) + 1,15,h_activaty_func=torch.relu)
+    # # opt_SGD = torch.optim.SGD(model.parameters(), lr=0.01)
+    # outs_wine = model_voice.trains(x_train_std, y_train, maxiter=300, learning_rate=0.1, self_structure = False)
+    #
+    # model_voice.predict(x_train_std, y_train)
+    # print("训练集准确率",model_voice.evaluate())
+    #
+    # model_voice.predict(x_test_std, y_test)
+    # print("测试集准确率",model_voice.evaluate())
+    # # print(outs)
+    # plot(np.array(model_voice.record_hidden))
+    # print(model_voice.n_hidden)
+    #
+
+
+
+
+
